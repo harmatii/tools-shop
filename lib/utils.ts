1b0d727943287960e327c116a2b2e0252d12dcd4
Ihor <ihor.harmatii@gmail.com>
2026-03-13 22:03:03 +0200

    added zod validator and Product type

--- lib/utils.ts
+++ lib/utils.ts
@@ -7,6 +7,7 @@
 
 import { clsx, type ClassValue } from "clsx";
 import { twMerge } from "tailwind-merge";
+import { number } from "zod";
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
@@ -16,4 +17,10 @@ export function cn(...inputs: ClassValue[]) {
 
 export function convertToPlainObject<T>(value: T): T {
   return JSON.parse(JSON.stringify(value));
+}
+
+//Format number with decimal places
+export function formatNumberWithDecimal(num: number): string {
+  const [int, decimal] = num.toString().split(".");
+  return decimal ? `${int}.${decimal.padEnd(2, "0")}` : `${int}.00`;
 }
